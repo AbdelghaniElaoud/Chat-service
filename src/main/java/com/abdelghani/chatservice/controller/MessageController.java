0bd570d9e5b1ec6605a88b578d8084265e7c440d
abdelghanielaoud <abdelghanielaoud@gmail.com>
2025-03-12 09:47:30 +0000

    Users are showing !!

--- src/main/java/com/abdelghani/chatservice/controller/MessageController.java
+++ src/main/java/com/abdelghani/chatservice/controller/MessageController.java
@@ -1,11 +1,11 @@
 package com.abdelghani.chatservice.controller;
 
 import com.abdelghani.chatservice.entities.Message;
+import com.abdelghani.chatservice.entities.User;
 import com.abdelghani.chatservice.repository.MessageRepository;
+import com.abdelghani.chatservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +16,23 @@ public class MessageController {
     @Autowired
     private MessageRepository messageRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
     public List<Message> getAllMessages() {
         return messageRepository.findAll();
+    }
+
+    @GetMapping("/contacts")
+    public List<User> getAllContacts() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{userId1}/{userId2}")
+    public List<Message> getMessagesBetweenUsers(@PathVariable Long userId1, @PathVariable Long userId2) {
+        User user1 = userRepository.findById(userId1).orElseThrow(() -> new RuntimeException("User not found"));
+        User user2 = userRepository.findById(userId2).orElseThrow(() -> new RuntimeException("User not found"));
+        return messageRepository.findBySenderAndRecipientOrRecipientAndSender(user1, user2, user2, user1);
     }
 }
